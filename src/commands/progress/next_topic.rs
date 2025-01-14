94c65e2a1689e5cc1799ed7630f51386d6b2f442
Almaz Murzabekov <almaz@murzabekov.net>
2025-01-14 16:46:10 +0100

    feat: next command

--- src/commands/progress/next_topic.rs
+++ src/commands/progress/next_topic.rs
@@ -7,7 +7,15 @@ use crate::{
 };
 
 #[derive(Debug, Args)]
-pub struct NextTopicCommand {}
+pub struct NextTopicCommand {
+    /// Mark current topic as completed
+    #[arg(short, long, default_value_t = false)]
+    pub mark_completed: bool,
+
+    /// Skip completed topics
+    #[arg(long, default_value_t = true)]
+    pub skip_completed: bool,
+}
 
 impl CommandExecutor for NextTopicCommand {
     fn execute(&self, _context: &mut CommandContext) -> Result<(), DbCheckError> {
